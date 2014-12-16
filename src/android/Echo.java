4d9c77494012c682a55a950ac2a5f7ba3ff56a8e
Vitor Venturin Linhalis <vitorvl@yahoo.com.br>
2014-12-16 00:37:38 -0200

    v4

--- src/android/Echo.java
+++ src/android/Echo.java
@@ -10,15 +10,15 @@ import org.json.JSONObject;
 import android.util.Log;
 
 public class Echo extends CordovaPlugin {
+    private static final String TAG = "MyActivity";
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("escrever")) {
             String message = args.getString(0);
-            Log.i('Echo', message);
-            Log.i('Echo', message);
-            Log.i('Echo', message);
-            Log.i('Echo', message);
+
+            Log.d(TAG, message);
+            
             this.echo(message, callbackContext);
             return true;
         }
