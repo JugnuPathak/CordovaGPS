f92afac1981bdf4db1ee44605f050a5d7cb7035a
Vitor Venturin Linhalis <vitorvl@yahoo.com.br>
2014-12-17 12:31:41 -0200

    alert(resp)

--- www/echo.js
+++ www/echo.js
@@ -3,10 +3,11 @@ var exec = require('cordova/exec');
 var echo = {
     echoNow: function() {  
     	var success = function(resp) { 
-    		var c = document.getElementById("container")
-            var newParagraph = document.createElement('p');
-            newParagraph.innerHTML = '('+ resp[0] + ',' + resp[1] + ')';
-            c.appendChild(newParagraph);
+    		// var c = document.getElementById("container")
+      //       var newParagraph = document.createElement('p');
+      //       newParagraph.innerHTML = '('+ resp[0] + ',' + resp[1] + ')';
+      //       c.appendChild(newParagraph);
+      alert(resp);
     	};
         var error = function(message) { alert("Oopsie! " + message); };
         cordova.exec(success, error, "Echo", "escrever", []);
