36af997c3864eb0dc012c49da05c16a43663e900
Vitor Venturin Linhalis <vitorvl@yahoo.com.br>
2014-12-17 12:46:08 -0200

    plugin.xml sources

--- src/android/Echo.java
+++ src/android/Echo.java
@@ -8,5 +8,26 @@ import org.json.JSONException;
 import android.util.Log;
 
 public class Echo extends CordovaPlugin {
-    
+    private static final String TAG = "DebugLogger";
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("escrever")) {
+//            String message = args.getString(0);
+        	
+        	JSONArray obj = new JSONArray();
+        	obj.put(HelloWorld.LAT);
+        	obj.put(HelloWorld.LNG);
+            
+            if (obj != null && obj.length() > 0) {
+                Log.d(TAG, obj.toString());
+                callbackContext.success(obj);
+            } else {
+                callbackContext.error("Expected one non-empty string argument.");
+            }
+            
+            return true;
+        }
+        return false;
+    }
 }
