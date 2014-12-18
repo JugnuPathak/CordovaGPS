3016d2b3d165e3403e8e45fbd27abec201e36960
Vitor Venturin Linhalis <vitorvl@yahoo.com.br>
2014-12-18 14:54:25 -0200

    com.vitorventurin.gps

--- src/example/TestForEcho/platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/Vitor/Documents/PHONEGAP/CordovaGPS/src/example/TestForEcho/platforms/android/ant-build/AndroidManifest.xml : \
-/Users/Vitor/Documents/PHONEGAP/CordovaGPS/src/example/TestForEcho/platforms/android/AndroidManifest.xml \
--- src/example/TestForEcho/platforms/android/ant-build/HelloWorld-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/Vitor/Documents/PHONEGAP/CordovaGPS/src/example/TestForEcho/platforms/android/ant-build/HelloWorld-debug-unaligned.apk : \
-/Users/Vitor/Documents/PHONEGAP/CordovaGPS/src/example/TestForEcho/platforms/android/ant-build/HelloWorld.ap_ \
-/Users/Vitor/Documents/PHONEGAP/CordovaGPS/src/example/TestForEcho/platforms/android/ant-build/classes.dex \
-/Users/Vitor/Documents/PHONEGAP/CordovaGPS/src/example/TestForEcho/platforms/android/CordovaLib/ant-build/classes.jar \
