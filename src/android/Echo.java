3f208cf6da82aab193ca9656dd47ca59cbbbabff
Vitor Venturin Linhalis <vitorvl@yahoo.com.br>
2014-12-18 12:13:06 -0200

    VVLTDA

--- src/android/Echo.java
+++ src/android/Echo.java
@@ -5,24 +5,34 @@ import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.phonegap.helloworld.HelloWorld;
 
-public class Echo extends CordovaPlugin {
-    private static final String TAG = "DebugLogger";
+public class Echo extends CordovaPlugin implements LocationListener {
+    private LocationManager locationManager;
+    private double lat;
+    private double lng;
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("escrever")) {
-//            String message = args.getString(0);
+            locationManager = (LocationManager) this.webView.getContext().getSystemService(Context.LOCATION_SERVICE);
+            
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                    3000, 5, this);
             
             JSONArray obj = new JSONArray();
-            obj.put(HelloWorld.LAT);
-            obj.put(HelloWorld.LNG);
+            obj.put(lat);
+            obj.put(lng);
             
             if (obj != null && obj.length() > 0) {
-                Log.d(TAG, obj.toString());
                 callbackContext.success(obj);
             } else {
                 callbackContext.error("Expected one non-empty string argument.");
@@ -31,5 +41,26 @@ public class Echo extends CordovaPlugin {
             return true;
         }
         return false;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        this.lat = location.getLatitude();
+        this.lng = location.getLongitude();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Toast.makeText(this.webView.getContext(), "Gps turned on ", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(this.webView.getContext(), "Gps turned off ", Toast.LENGTH_LONG).show(); 
     }
 }
